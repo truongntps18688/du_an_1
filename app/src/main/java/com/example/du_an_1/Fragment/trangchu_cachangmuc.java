fab813c267c93903e011a4d2f822bf2c42597978
truongntps18688 <81845787+truongntps18688@users.noreply.github.com>
2021-11-23 22:16:07 +0700

    ms_nhat_nhe
    abc

--- app/src/main/java/com/example/du_an_1/Fragment/trangchu_cachangmuc.java
+++ app/src/main/java/com/example/du_an_1/Fragment/trangchu_cachangmuc.java
@@ -11,9 +11,17 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.du_an_1.R;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class trangchu_cachangmuc extends Fragment {
-
+    private BarChart barchart;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,5 +35,28 @@ public class trangchu_cachangmuc extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        barchart = view.findViewById(R.id.barchart);
+        List<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(0f, 30f));
+        entries.add(new BarEntry(1f, 80f));
+        entries.add(new BarEntry(2f, 60f));
+        entries.add(new BarEntry(3f, 50f));
+        entries.add(new BarEntry(4f, 50f));
+        entries.add(new BarEntry(5f, 70f));
+        entries.add(new BarEntry(6f, 60f));
+        entries.add(new BarEntry(7f, 80f));
+        entries.add(new BarEntry(8f, 100f));
+        entries.add(new BarEntry(9f, 90f));
+        entries.add(new BarEntry(10f, 10f));
+        entries.add(new BarEntry(11f, 70f));
+        entries.add(new BarEntry(12f, 10f));
+
+        BarDataSet set = new BarDataSet(entries,"thống kê");
+        BarData data = new BarData(set);
+        data.setBarWidth(0.9f);  // khoảng cách <1/ càng nhỏ thì khoảng cách càng lớn
+        barchart.setData(data); // set dữ liệu
+        barchart.setFitBars(true); // làm cho trục x khớp chính xác với tất cả các
+        barchart.invalidate(); // tạo ms
+
     }
 }
--- app/src/main/java/com/example/du_an_1/Fragment/trangchu_tongquan.java
+++ app/src/main/java/com/example/du_an_1/Fragment/trangchu_tongquan.java
@@ -5,18 +5,34 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.du_an_1.R;
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
+import az.plainpie.PieView;
+import az.plainpie.animation.PieAngleAnimation;
 
 public class trangchu_tongquan extends Fragment {
-
+    private PieChart pieChart;
+    private PieView pieView;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
     @Nullable
     @Override
@@ -27,5 +43,61 @@ public class trangchu_tongquan extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        pieChart = view.findViewById(R.id.activity_main_piechart);
+        setupPieChart();
+        loadPieChartData();
+
+
+
+        pieView = view.findViewById(R.id.pieView);
+        pieView.setMainBackgroundColor (getResources (). getColor (R.color.black));// thay đổi màu nền
+        pieView.setPercentageBackgroundColor(getResources().getColor(android.R.color.holo_blue_bright));// thay đổi màu thanh phân trăm
+        pieView.setPieInnerPadding(30); // số càng lớn thì thanh tròn càng lớn // Thay đổi độ dày của thanh phần trăm
+        pieView.setPercentage(70);// set %
+        pieView.setPercentageTextSize(20); // set size %
+
+    }
+    private void setupPieChart() {
+        pieChart.setDrawHoleEnabled(true);
+        pieChart.setUsePercentValues(true); // tính phần trăm
+        pieChart.setEntryLabelTextSize(12);
+        pieChart.setEntryLabelColor(Color.BLACK);
+        pieChart.setCenterTextSize(24);
+        pieChart.getDescription().setEnabled(false);
+
+        Legend l = pieChart.getLegend();
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);// chỉ sửa chỗ hiển thị bảng chú thích
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT);
+        l.setOrientation(Legend.LegendOrientation.VERTICAL);
+        l.setDrawInside(false);
+        l.setEnabled(false); // tắt mở bảng chú thích
+    }
+    private void loadPieChartData() {
+        ArrayList<PieEntry> entries = new ArrayList<>();
+        entries.add(new PieEntry(0.25f));
+        entries.add(new PieEntry(0.3f));
+
+        ArrayList<Integer> colors = new ArrayList<>();
+        for (int color: ColorTemplate.MATERIAL_COLORS) {
+            colors.add(color);
+        }
+
+        for (int color: ColorTemplate.VORDIPLOM_COLORS) {
+            colors.add(color);
+        }
+
+        PieDataSet dataSet = new PieDataSet(entries, "Expense Category");
+        dataSet.setColors(colors);
+
+        PieData data = new PieData(dataSet);
+        data.setDrawValues(true); // hiển thị % hay không
+        data.setValueFormatter(new PercentFormatter(pieChart));
+        data.setValueTextSize(12f);
+        data.setValueTextColor(Color.BLACK);
+
+        pieChart.setData(data);
+        pieChart.invalidate();
+
+        pieChart.animateY(2000, Easing.EaseInOutQuad);
     }
 }
